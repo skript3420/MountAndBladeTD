0f27f64b5d108feabc9493137aeb1e0fd3e6871e
David Mack <uinbg@student.kit.edu>
2025-10-31 22:53:04 +0100

    add WSE-addons

--- source/Module_system 1.171/process_operations.py
+++ source/Module_system 1.171/process_operations.py
@@ -429,11 +429,24 @@ def save_statement_block(ofile,statement_name,can_fail_statement,statement_block
     else:
       opcode = statement[0]
       no_variables = 0
+    #---ORIGINAL START---  
+    #if (opcode in [try_begin,
+    #               try_for_range,
+    #               try_for_range_backwards,
+    #               try_for_parties,
+    #               try_for_agents]):
+    #---ORIGINAL END---
+    #---WSE START---
     if (opcode in [try_begin,
                    try_for_range,
                    try_for_range_backwards,
                    try_for_parties,
-                   try_for_agents]):
+                   try_for_agents,
+                   try_for_prop_instances,
+                   try_for_players,
+                   try_for_dict_keys,
+                   ]):
+    #---WSE END---
       current_depth = current_depth + 1
     elif (opcode == try_end):
       current_depth = current_depth - 1
